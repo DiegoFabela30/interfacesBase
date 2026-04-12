e1053156c63d25767a3063fd7f118b9d408a66d7
Ruben Alejandro <nolascoalex106@gmail.com>
2026-04-11 20:56:57 -0600

    Correccion Footer
    Duplicación de codigo

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -29,14 +29,3 @@
     </p>
   </div>
 </footer>
-
-<footer class="dashboard-footer institutional-footer">
-  <div class="footer-content">
-    <p>Copyright © {{ now()->year }} G.A.M.A Solutions. Todos los derechos reservados.</p>
-    <p class="footer-links">
-      <a href="{{ route('terms') }}">Términos y condiciones</a>
-      <span class="footer-link-separator">|</span>
-      <a href="{{ url('aviso-de-privacidad') }}">Aviso de privacidad</a>
-    </p>
-  </div>
-</footer>
